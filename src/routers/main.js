e6ab1e6f57d5e559f704e479143f5b839eaef50a
suggestied <74950245+suggestied@users.noreply.github.com>
2022-08-23 07:46:14 -0400

    Scroll to top on load

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,12 +1,25 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Error404 from "../pages/404";
 import Home from "../pages/Home";
 import PortfolioRouter from "./portfolio";
 
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export default function MainRouter() {
   return (
     <Router>
+      <ScrollToTop />
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       <Switch>
